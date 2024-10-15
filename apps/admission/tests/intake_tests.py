48d36bea9e9b2df54f37e4662d742627cc2bd4b1
Mark <markfindsen@gmail.com>
2024-10-15 19:40:12 +1000

    Split Course/Intake Unit Tests Up

--- apps/admission/tests/intake_tests.py
+++ apps/admission/tests/intake_tests.py
@@ -1,30 +1,8 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from .models import Course, Intake
+
+from ..models import Course, Intake
 from datetime import date
-
-class CourseModelTestCase(TestCase):
-    def setUp(self):
-        self.valid_name = "CSSE2310"
-
-    def test_course_requires_name(self):
-        with self.assertRaises(ValidationError):
-            course = Course()
-            course.full_clean()
-
-        course = Course(name=self.valid_name)
-        try:
-            course.full_clean()
-        except ValidationError:
-            self.fail("full_clean() raised ValidationError unexpectedly!")
-
-    def test_course_default_output(self):
-        course = Course.objects.create(name=self.valid_name)
-        self.assertEqual(str(course), self.valid_name)
-
-    def test_course_name_max_length(self):
-        max_length = Course._meta.get_field('name').max_length
-        self.assertEqual(max_length, 50)
 
 class IntakeModelTestCase(TestCase):
     def setUp(self):
